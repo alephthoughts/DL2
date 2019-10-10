ee84d8f01ff65d4338df3180e8314ed2cd9d12f6
alephthoughts <abhisheksharma09@gmail.com>
2019-10-10 19:29:03 +0530

    02 done

--- exp/nb_01.py
+++ exp/nb_01.py
@@ -12,5 +12,13 @@ def test(a,b,cmp,cname=None):
 
 def test_eq(a,b): test(TEST,'Test',operator.eq, '==')
 
+from pathlib import Path
+from IPython.core.debugger import set_trace
+from fastai import datasets
+import pickle, gzip, math, torch, matplotlib as mpl
+import matplotlib.pyplot as plt
+from torch import tensor
+MNIST_URL='http://deeplearning.net/data/mnist/mnist.pkl'
+
 def near(a, b): return torch.allclose(a, b, rtol=1e-3, atol=1e-5)
 def test_near(a,b): return test(a, b, near)
